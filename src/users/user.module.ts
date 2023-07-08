ff290122ada50af3d4b65f824ac333990dd66dc8
Rian Lucas <rianluccas08@gmail.com>
2023-07-08 15:15:40 -0300

    build: (Users) provide repository and interface to module

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/infra/database/prisma.service';
+import { IUserRepository } from './repositories/user.repository';
+import { UserPrismaRepository } from './repositories/prisma/user.prisma.repositoty';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [
+    UserService,
+    PrismaService,
+    {
+      provide: IUserRepository,
+      useClass: UserPrismaRepository,
+    },
+  ],
 })
 export class UserModule {}
