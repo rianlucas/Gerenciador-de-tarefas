b2737cb0de2098b649d4eb3aebd2f94228cb3792
Rian Lucas <rianluccas08@gmail.com>
2023-07-10 17:21:36 -0300

    feat: (Global) add Login module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, LoginModule],
   controllers: [],
   providers: [],
 })
